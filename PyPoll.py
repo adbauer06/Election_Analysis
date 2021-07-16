079ec3985e79fcdd2e0c107777a0b83484ae318d
adbauer06 <86528761+adbauer06@users.noreply.github.com>
2021-07-15 21:33:29 -0500

    Add files via upload
    Uploaded PyPoll file

---
+++ PyPoll.py
@@ -0,0 +1,42 @@
+# The date we need to retrieve:
+# 1.  The total number of votes cast
+# 2.  A complete list of candidates who recieved votes
+# 3.  The percentage of votes each candidate won
+# 4.  The total number of votes each candidate won
+# 5.  The winner of the election based on popular vote
+
+import csv
+import os
+
+# Assign a variable to load a file form a path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+
+    # To do:  read and analyze the data here.
+    #  Read the file object with the reader function.
+    file_reader = csv.reader(election_data)
+
+    # Read and print the header row
+    headers = next(file_reader)
+    # print(headers)
+
+    #Print each row in the CSV file.
+    for row in file_reader:
+        print(row)
+
+
+
+# Using the with statement open the file as a text file.
+#with open(file_to_save, "w") as txt_file:
+# Write some data to the file.
+#    txt_file.write("Counties in the Election\n")
+#    txt_file.write("--------------------------\n")
+#    txt_file.write("Arapahoe\nDenver\nJefferson")
+    
+
+
